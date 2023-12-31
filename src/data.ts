6071a69daa2c8522995e998b04de05bbef4773d8
sehyeon An <sehyeon014@gmail.com>
2023-12-31 18:15:24 +0900

    ⚡️  2022->2023, modify some values
    가장 많이 사용된 태그 수정 필요

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,12 @@
 export const Weekday = [
   //
-  { key: '월', value: 512 },
-  { key: '화', value: 495 },
-  { key: '수', value: 477 },
-  { key: '목', value: 414 },
-  { key: '금', value: 255 },
-  { key: '토', value: 267 },
-  { key: '일', value: 418 },
+  { key: '월', value: 2441 },
+  { key: '화', value: 2614 },
+  { key: '수', value: 2586 },
+  { key: '목', value: 2270 },
+  { key: '금', value: 1386 },
+  { key: '토', value: 1140 },
+  { key: '일', value: 1809 },
 ];
 
 export const Tags = [
